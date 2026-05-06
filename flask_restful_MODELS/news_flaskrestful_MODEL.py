8d04972ca7ccccb5396db4fc672e589b859e77ff
Vinz38 <ilia.nikulin09@gmail.com>
2026-05-06 22:18:59 +0300

    Модель для получения, создания и удаления новостей сервером

---
+++ flask_restful_MODELS/news_flaskrestful_MODEL.py
@@ -0,0 +1,53 @@
+from flask_restful import Resource, reqparse, abort
+from data import db_session
+from data.news import News
+import flask 
+
+def abort_if_news_not_found(news_id):
+    session = db_session.create_session()
+    news = session.query(News).get(news_id)
+    if not news:
+        abort(404, message=f"News {news_id} not found")
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('link_news', required=True, help="Link cannot be blank!")
+parser.add_argument('users', required=True, help="Users cannot be blank!")
+parser.add_argument('categories', required=True, help="Categories cannot be blank!")
+
+
+class NewsResource(Resource):
+    def get(self, news_id):
+        abort_if_news_not_found(news_id)
+        session = db_session.create_session()
+        news = session.query(News).get(news_id)
+        return news.to_dict(
+            only=('id', 'link_news', 'users', 'categories'))
+
+    def delete(self, news_id):
+        abort_if_news_not_found(news_id)
+        session = db_session.create_session()
+        news = session.query(News).get(news_id)
+        session.delete(news)
+        session.commit()
+        return flask.jsonify({'success': 'OK'})
+    
+
+class NewsListResource(Resource):
+    def get(self):
+        session = db_session.create_session()
+        news = session.query(News).all()
+        return flask.jsonify({'news': [item.to_dict(
+            only=('id', 'link_news', 'users', 'categories')) for item in news]})
+
+    def post(self):
+        args = parser.parse_args()
+        session = db_session.create_session()
+        news = News(
+            link_news=args['link_news'],
+            users=args['users'],
+            categories=args['categories']
+        )
+        session.add(news)
+        session.commit()
+        return flask.jsonify({'success': 'OK'})
