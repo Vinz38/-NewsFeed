043382b93c17ec7d4872550191e01a2c77b12c49
Vinz38 <ilia.nikulin09@gmail.com>
2026-05-07 21:09:58 +0300

    Добавление в rest api функции изменения новостей

--- flask_restful_MODELS/news_flaskrestful_MODEL.py
+++ flask_restful_MODELS/news_flaskrestful_MODEL.py
@@ -1,7 +1,8 @@
 from flask_restful import Resource, reqparse, abort
 from data import db_session
 from data.news import News
-import flask 
+import flask
+
 
 def abort_if_news_not_found(news_id):
     session = db_session.create_session()
@@ -13,7 +14,8 @@ def abort_if_news_not_found(news_id):
 parser = reqparse.RequestParser()
 parser.add_argument('link_news', required=True, help="Link cannot be blank!")
 parser.add_argument('users', required=True, help="Users cannot be blank!")
-parser.add_argument('categories', required=True, help="Categories cannot be blank!")
+parser.add_argument('categories', required=True,
+                    help="Categories cannot be blank!")
 
 
 class NewsResource(Resource):
@@ -31,7 +33,26 @@ class NewsResource(Resource):
         session.delete(news)
         session.commit()
         return flask.jsonify({'success': 'OK'})
-    
+
+    def put(self, news_id):
+        args = parser.parse_args()
+        session = db_session.create_session()
+        news = session.query(News).get(news_id)
+        if news:
+            news.link_news = args['link_news']
+            news.users = args['users']
+            news.categories = args['categories']
+        else:
+            news = News(
+                id=news_id,
+                link_news=args['link_news'],
+                users=args['users'],
+                categories=args['categories']
+            )
+            session.add(news)
+        session.commit()
+        return flask.jsonify({'success': 'OK'})
+
 
 class NewsListResource(Resource):
     def get(self):
